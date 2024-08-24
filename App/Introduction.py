70b9fbe45e1f8f1a0764ece7457e466bed73b3bd
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-24 12:33:24 -0600

    Update Introduction.py
    Cambio de markdown para un mejor formateo del texto

--- App/Introduction.py
+++ App/Introduction.py
@@ -3,51 +3,43 @@ import streamlit as st
 st.set_page_config(
     page_title="Introduction",
     page_icon="🤝🏼",
+    layout="wide",    
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
 )
 
 st.write("# ¡Bienvenido! 🤝🏼")
 
 st.markdown(
     """
-    ### Bienvenido a UDGPracPhy
-    ## ¡Bienvenido a UDGPracPhy, tu plataforma de estudio y práctica para el curso de Álgebra Lineal I para la licenciatura en Física en la Universidad de Guadalajara!
+    #### ¡Bienvenido a Pracphy, tu plataforma de estudio y práctica para la licenciatura en Física en la Universidad de Guadalajara!
     ### ¿Qué es Álgebra Lineal I?
-    ## Álgebra Lineal I es una unidad de aprendizaje fundamental en la Licenciatura en Física. Este curso te introducirá a conceptos clave como:
-    
-    - Matrices
-    - Determinantes
-    - Espacios vectoriales
-    - Transformaciones lineales
+    #### Álgebra Lineal I es una unidad de aprendizaje fundamental para la carrera. Este curso te introducirá a conceptos clave como: Vectores, Independencia Lineal, Matriz Escalonada, Eigenvalues y mucho más. Estos conocimientos son esenciales para comprender y modelar una amplia gama de fenómenos físicos y matemáticos.
+    ## ¿Qué aprenderás?
+    #### A lo largo de esta página web, explorarás seis unidades temáticas principales:
 
-    Estos conocimientos son esenciales para comprender y modelar una amplia gama de fenómenos físicos y matemáticos.
-    ### ¿Qué aprenderás?
-    ## A lo largo de este curso, explorarás cinco unidades temáticas principales:
-
-    - Introducción al álgebra vectorial
-    - Matrices y determinantes
-    - Espacios vectoriales
-    - Sistemas de ecuaciones lineales
-    - Transformaciones lineales
+    1. [Vectores](https://algebralinear.streamlit.app/Vectores)
+    2. [Matrices y Determinantes](https://algebralinear.streamlit.app/Matrices_y_Determinantes)
+    3. [Espacios Vectoriales](https://algebralinear.streamlit.app/Espacios_Vectoriales)
+    4. [Sistemas de Ecuaciones Lineales](https://algebralinear.streamlit.app/Sistemas_de_Ecuaciones_Lineales)
+    5. [Transformaciones Lineales](https://algebralinear.streamlit.app/Transformaciones_Lineales)
+    6. [Diagonalización](https://algebralinear.streamlit.app/Diagonalizacion)
 
     Cada unidad te proporcionará las herramientas necesarias para desarrollar tu pensamiento lógico-matemático y aplicar estos conceptos en diversos ámbitos de la física y las matemáticas.
     ### ¿Cómo usar esta plataforma?
-    AlgebraLinealUDG está diseñada para complementar tus clases presenciales. Aquí encontrarás:
+    Pracphy está diseñada para complementar tus clases presenciales. Aquí encontrarás:
 
     - Resúmenes teóricos de cada unidad
     - Ejercicios prácticos con soluciones paso a paso
-    - Videos explicativos de conceptos clave
-    - Cuestionarios de autoevaluación
+    - Colección de videos explicativos
+    - Descarga de ejercicios prácticos
     - Recursos adicionales para profundizar en los temas
     
-    Nuestro objetivo es ayudarte a desarrollar las competencias necesarias para comprender y aplicar los métodos del Álgebra Lineal en tu futuro profesional como físico.
+    Nuestro objetivo es ayudarte a desarrollar las competencias necesarias para comprender y aplicar los métodos del Álgebra Lineal en tu carrera universitaria.
     
-    ¡Comienza tu viaje por el fascinante mundo del Álgebra Lineal y descubre cómo estas herramientas matemáticas pueden ayudarte a entender mejor el universo que nos rodea! - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    ¡Descubre cómo estas herramientas matemáticas pueden ayudarte a entender mejor el universo que nos rodea pero hasta que hayas entendido esto! 
 """
 )

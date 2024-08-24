c6728537149c248f998a9c826f12ea6d0ca99c5e
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-24 14:16:31 -0600

    Update and rename 4_System_of_Linear_Equations.py to 4_🪜_Sistema_de_Ecuaciones_Lineales.py

--- App/pages/4_🪜_Sistema_de_Ecuaciones_Lineales.py
+++ App/pages/4_🪜_Sistema_de_Ecuaciones_Lineales.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="System of Linear Equations",
-    page_icon="✍🏼​",
+    page_title="Sistema de Ecuaciones Lineales",
+    page_icon="🪜​",
 )
 
 st.write("# Aqui va lo de vectores ")

b5159edf9db53347e818b30683750a25cefbf756
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-24 14:07:14 -0600

    Update 3_👽_Espacios_Vectoriales.py

--- App/pages/3_👽_Espacios_Vectoriales.py
+++ App/pages/3_👽_Espacios_Vectoriales.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Espacios Vectoriales",
-    page_icon=:milky_way:​,
+    page_icon=":milky_way:"​,
 )
 
 st.write("# Aqui va lo de vectores ")

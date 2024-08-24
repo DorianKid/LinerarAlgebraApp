589926e13af4d32b8d87f2276a5a0ed3b34c0ea4
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-24 14:18:56 -0600

    Update and rename 5_💪_Linear_Transformations.py to 5_🪄_Linear_Transformations.py

--- App/pages/5_🪄_Linear_Transformations.py
+++ App/pages/5_🪄_Linear_Transformations.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Linear Transformations",
-    page_icon="💪",
+    page_icon="🪄",
 )
 
 st.write("# Aqui va lo de transformaciones lineales ")

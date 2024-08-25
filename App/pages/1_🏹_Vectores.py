ce0a472cd1fc442a2f9f8e843f32f59a6dde86d6
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-25 06:28:54 -0600

    Update 1_🏹_Vectores.py

--- App/pages/1_🏹_Vectores.py
+++ App/pages/1_🏹_Vectores.py
@@ -11,4 +11,29 @@ st.set_page_config(
         'About': "Todo bien, ¡suerte!"}
 )
 
-st.markdown("# Aqui va lo de vectores en $$\mathbb{R}^{2}$$ and $$\mathbb{R}^{3}$$")
+if st.session_state.page == 'generate':
+    st.title("Vectores")
+    st.header("Definiciones")
+
+    ### OTRAS REFERENCIAS 
+    # Kurosch, A. G. (1968). *Curso de álgebra superior*. Moscow: Mir Publishers (Издательство "Мир")
+
+    # Matrix's order 
+    st.subheader("Vector Columna y Vector Renglón")
+    st.markdown('''
+**Definición**: Un :red[vector de n componentes] se define como un conjunto ordenado de :red[n] números escritos de la siguiente manera:
+$$\begin{bmatrix}
+   a \\
+   b \\
+   c
+\end{bmatrix}$$
+Grossman, S. (2012). *Álgebra Lineal*. (7$^{\\textrm{ma}}$ ed., pp. 48) McGraw Hill. 
+    ''')
+
+    # Matrix's elements 
+    st.subheader("Elements of the Matrix")
+    st.markdown('''
+    **Definition**: The elements of a matrix are the individual values that make up the matrix. If $A$ is a matrix of order $m \\times n$, then each element $a_{ij}$ of $A$ is located at the intersection of the $i$-th row and the $j$-th column.
+
+Grossman, S. (2012). *Álgebra Lineal*. (7$^{\\textrm{ma}}$ ed., pp. 48) McGraw Hill. 
+    ''')

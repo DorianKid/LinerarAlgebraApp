43a3508f652ed27e43dbc77f03e9502994c75695
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-25 06:39:08 -0600

    Update 1_🏹_Vectores.py

--- App/pages/1_🏹_Vectores.py
+++ App/pages/1_🏹_Vectores.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(
     page_title="Vectores",
     page_icon="🏹",  
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded
+    ",
     layout="wide",
     menu_items={
         'Get Help': 'https://www.extremelycoolapp.com/help',

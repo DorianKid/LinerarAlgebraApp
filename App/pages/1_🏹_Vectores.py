4728733e89803361c8e92faaf59a11785ad54764
Dario Abraham Curiel Niño <130793949+DorianKid@users.noreply.github.com>
2024-08-25 06:18:02 -0600

    Update 1_🏹_Vectores.py

--- App/pages/1_🏹_Vectores.py
+++ App/pages/1_🏹_Vectores.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Vectores",
-    page_icon="🏹",
+    page_icon="🏹",  
+    initial_sidebar_state="collapsed",
+    layout="wide",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "Todo bien, ¡suerte!"}
 )
 
 st.markdown("# Aqui va lo de vectores en $$\mathbb{R}^{2}$$ and $$\mathbb{R}^{3}$$")
